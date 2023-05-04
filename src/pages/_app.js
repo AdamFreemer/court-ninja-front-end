404ea47a56ddf71052c69ac9405ef22095cfbeb6
Adam Freemer <adam@freemer.com>
2023-05-03 21:05:24 -0700

    Stubbed in auth0-1

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,12 +3,10 @@ import '@/styles/globals.css'
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 export default function App({ Component, pageProps }) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
 
-    return (
-      <UserProvider>
-        <Component {...pageProps} />
-      </UserProvider>
-
-    ) 
-
+  ) 
 }
